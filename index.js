8610781942c8fe804ec59ffa990225f415633317
Yassine Mili <ya.mili@icloud.com>
2025-03-20 10:57:38 +0100

    add all the routes

--- index.js
+++ index.js
@@ -5,24 +5,27 @@ const connectDB = require("./config/db");
 
 require("dotenv").config();
 
-
 const userRoutes = require("./routes/userRoutes");
 const teamRoutes = require("./routes/teamRoutes");
 const competitionRoutes = require("./routes/competitionRoutes");
+const challengeRoutes = require("./routes/challengeRoutes");
+const submissionRoutes = require("./routes/submissionRoutes");
+const scoreRoutes = require("./routes/scoreRoutes");
 
 const app = express();
-
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
 
-
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/teams", teamRoutes);
 app.use("/api/competitions", competitionRoutes);
+app.use("/api/challenges", challengeRoutes);
+app.use("/api/submissions", submissionRoutes);
+app.use("/api/scores", scoreRoutes);
 
-// Start Server
 const PORT = process.env.PORT || 5000;
 connectDB();
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));

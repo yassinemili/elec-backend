6b77ff701f4546c8a094c7ce5d019ef6b0b7aeaf
Yassine Mili <ya.mili@icloud.com>
2025-03-21 08:30:28 +0100

    add the get comp rank routes

--- routes/competitionRoutes.js
+++ routes/competitionRoutes.js
@@ -5,17 +5,24 @@ const {
     getAllCompetitions,
     getCompetitionById,
     updateCompetition,
-    deleteCompetition
+    deleteCompetition,
+    getCompetitionRankings
 } = require("../controllers/competitionController");
 
 router.route("/")
     .get(getAllCompetitions)
     .post(createCompetition);
 
+
+
 router.route("/:id")
     .get(getCompetitionById)
     .put(updateCompetition)
     .delete(deleteCompetition);
+
+router.route("/rankings/:id")
+    .get(getCompetitionRankings);
+
 
 
 module.exports = router;

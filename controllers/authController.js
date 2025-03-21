3bc00250eada4b8d6bf33192ae3f6598de604be5
Yassine Mili <ya.mili@icloud.com>
2025-03-21 10:33:37 +0100

    add the logic for token in the login func

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = reauire("../models/userModel.js");
 const Team = reauire("../models/teamModel.js");
 const bcrypt = require("bcrypt");
+const { generateAccessToken } = require("../utils/tokenUtils");
 
 const login = async (req, res) => {
     const { name, password } = req.body;
@@ -17,17 +18,18 @@ const login = async (req, res) => {
 
         if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
 
+        const accessToken = await generateAccessToken({ userId: user._id, role: user.role });
+
+        res.cookie('token', accessToken, { httpOnly: true, secure: true });
         res.json({
             message: "Logged in successfully",
             user: {
                 userId: user._id,
-                role: user.role,
                 userName: user.name
             },
             team: {
                 teamId: team._id,
                 teamName: team.name,
-                
             },
         });
     } catch (error) {
@@ -35,29 +37,6 @@ const login = async (req, res) => {
     }
 };
 
-
-// Register user
-const register = async (req, res) => {
-    const { name, password } = req.body;
-
-    try {
-        const existingUser = await User.findOne({ name });
-
-        if (existingUser) return res.status(400).json({ message: "User already exists" });
-
-        const hashedPassword = await bcrypt.hash(password, 12);
-
-        const user = new User({ name, passwordHash: hashedPassword });
-
-        await user.save();
-
-        res.status(201).json({ message: "User registered successfully", user });
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error });
-    }
-};
-
-// Reset password
 const resetPassword = async (req, res) => {
     const { name, newPassword } = req.body;
 
@@ -80,6 +59,5 @@ const resetPassword = async (req, res) => {
 
 module.exports = {
     login,
-    register,
     resetPassword,
 };

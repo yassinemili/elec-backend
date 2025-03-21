3bc6c13029116ff2e184d972d6f935a295a789ca
Yassine Mili <ya.mili@icloud.com>
2025-03-21 08:22:20 +0100

    init the announcement model

---
+++ models/announcementModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const announcementSchema = new mongoose.Schema(
+    {
+        competitionId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Competition",
+            required: true
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Announcement", announcementSchema);

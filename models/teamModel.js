737d820b30dcefcdfea63d7ed60ac9c9400eb09b
Yassine Mili <ya.mili@icloud.com>
2025-03-23 04:08:33 +0100

    add the score field

--- models/teamModel.js
+++ models/teamModel.js
@@ -1,51 +1,55 @@
 const mongoose = require("mongoose");
 
 const teamSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    members: [
-      {
-        userId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
         },
-        role: {
-          type: String,
-          enum: ["member", "leader"],
-          default: "member",
-        },
-      },
-    ],
-    competitions: [
-      {
-        competitionId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Competition",
-        },
-        scores: [
-          {
-            challengeId: {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "Challenge",
-            },
-            score: {
-              scores: [
-                {
-                  type: mongoose.Schema.Types.ObjectId,
-                  ref: "Score",
+        members: [
+            {
+                userId: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "User",
                 },
-              ],
+                role: {
+                    type: String,
+                    enum: ["member", "leader"],
+                    default: "member",
+                },
             },
-          },
         ],
-      },
-    ],
-  },
-  { timestamps: true }
+        scores: {
+            type: Number,
+            default: 0,
+        }
+        /*     competitions: [
+              {
+                competitionId: {
+                  type: mongoose.Schema.Types.ObjectId,
+                  ref: "Competition",
+                },
+                scores: [
+                  {
+                    challengeId: {
+                      type: mongoose.Schema.Types.ObjectId,
+                      ref: "Challenge",
+                    },
+                    score: {
+                      scores: [
+                        {
+                          type: mongoose.Schema.Types.ObjectId,
+                          ref: "Score",
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            ], */
+    },
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("Team", teamSchema);

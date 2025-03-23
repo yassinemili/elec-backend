fcef87dc91e0378fce2447a62c0beb16d2705cf5
Yasser Benseghier <93477835+Yasser4Coder@users.noreply.github.com>
2025-03-23 02:57:26 +0100

    challengeController

--- controllers/challengeController.js
+++ controllers/challengeController.js
@@ -1,37 +1,55 @@
 const Challenge = require("../models/challengeModel");
 
 const getAllChallenges = async (req, res) => {
-    try {
-        const challenges = await Challenge.find()
-            .populate("competitionId", "name")
-            .populate("submissions.teamId", "name");
-        res.json(challenges);
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+  try {
+    const challenges = await Challenge.find()
+      .populate("competitionId", "name")
+      .populate("submissions.teamId", "name");
+    res.json(challenges);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 const createChallenge = async (req, res) => {
-    try {
-        const { competitionId, title, description, points, submissions } = req.body;
-        const challenge = new Challenge({ competitionId, title, description, points, submissions });
-        await challenge.save();
-        res.status(201).json(challenge);
-    } catch (error) {
-        res.status(400).json({ message: "Error creating challenge", error: error.message });
+  try {
+    if (
+      !req.body.title ||
+      !req.body.description ||
+      !req.body.points ||
+      !req.body.category
+    ) {
+      return res.status(400).json({ message: "some information are required" });
     }
+
+    const { title, description, points, hints, category } = req.body;
+    const challenge = new Challenge({
+      title,
+      description,
+      points,
+      hints,
+      category,
+    });
+    await challenge.save();
+    res.status(201).json(challenge);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error creating challenge", error: error.message });
+  }
 };
 
 const getChallengeById = async (req, res) => {
-    try {
-        const challenge = await Challenge.findById(req.params.id)
-            .populate("competitionId", "name")
-            .populate("submissions.teamId", "name");
-        if (!challenge) return res.status(404).json({ message: "Challenge not found" });
-        res.json(challenge);
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+  try {
+    const challenge = await Challenge.findById(req.params.id)
+      .populate("competitionId", "name")
+      .populate("submissions.teamId", "name");
+    if (!challenge)
+      return res.status(404).json({ message: "Challenge not found" });
+    res.json(challenge);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 };
 
 module.exports = { getAllChallenges, createChallenge, getChallengeById };
--- models/challengeModel.js
+++ models/challengeModel.js
@@ -1,49 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const challengeSchema = new mongoose.Schema(
-    {
-        competitionId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Competition",
-            required: true
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String
-        },
-        points: {
-            type: Number,
-            required: true
-        },
-        submissions: [
-            {
-                teamId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Team",
-                    required: true
-                },
-                submissionId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Submission",
-                    required: true
-                }
-            }
-        ],
-        isSolved: {
-            type: Boolean,
-            default: false
-        },
-        category: {
-            type: String,
-            enum: ["AI", "Graphic Design", "Problem Solving", "Cyber Security"],
-            required: true
-        }
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    description: {
+      type: String,
+      required: true,
+    },
+    points: {
+      type: Number,
+      required: true,
+    },
+    hints: {
+      type: [String],
+      default: [],
+    },
+    isSolved: {
+      type: Boolean,
+      default: false,
+    },
+    category: {
+      type: String,
+      enum: ["AI", "Graphic Design", "Problem Solving", "Cyber Security"],
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Challenge", challengeSchema);
-

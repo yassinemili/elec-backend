36515aa18bd46051ed429e50831ad698cfed92bd
Yassine Mili <ya.mili@icloud.com>
2025-03-24 06:07:10 +0100

    erore handling

--- controllers/challengeController.js
+++ controllers/challengeController.js
@@ -12,44 +12,38 @@ const getAllChallenges = async (req, res) => {
 
 const createChallenge = async (req, res) => {
   try {
-    if (
-      !req.body.title ||
-      !req.body.description ||
-      !req.body.points ||
-      !req.body.category
-    ) {
-      return res.status(400).json({ message: "some information are required" });
+    if (!req.body.title || !req.body.description || !req.body.points || !req.body.category) {
+      return res.status(400).json({ message: "Some information are required" });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ message: "File is required" });
     }
 
     // Upload file to Cloudinary
     const result = await cloudinary.uploader.upload(req.file.path, {
-      folder: "attechments",
+      folder: "attachments",
       resource_type: "auto",
       use_filename: true,
       unique_filename: false,
-      flags: "attachment",
     });
 
-    const downloadUrl = result.secure_url.replace(
-      "/upload/",
-      "/upload/fl_attachment/"
-    );
+    const downloadUrl = result.secure_url.replace("/upload/", "/upload/fl_attachment/");
 
     const { title, description, points, hints, category } = req.body;
     const challenge = new Challenge({
       title,
       description,
-      attechment: downloadUrl,
+      attachmentFile: downloadUrl,
       points,
       hints,
       category,
     });
+
     await challenge.save();
     res.status(201).json(challenge);
   } catch (error) {
-    res
-      .status(400)
-      .json({ message: "Error creating challenge", error: error.message });
+    res.status(400).json({ message: "Error creating challenge", error: error.message });
   }
 };
 

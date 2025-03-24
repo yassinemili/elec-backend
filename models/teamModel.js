fbc63dc6fa3fb4efd19190c441ab2169131961e7
Yassine Mili <ya.mili@icloud.com>
2025-03-24 06:10:36 +0100

    fixing the te total score field name

--- models/teamModel.js
+++ models/teamModel.js
@@ -1,55 +1,55 @@
 const mongoose = require("mongoose");
 
 const teamSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        members: [
-            {
-                userId: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "User",
-                },
-                role: {
-                    type: String,
-                    enum: ["member", "leader"],
-                    default: "member",
-                },
-            },
-        ],
-        scores: {
-            type: Number,
-            default: 0,
-        }
-        /*     competitions: [
-              {
-                competitionId: {
-                  type: mongoose.Schema.Types.ObjectId,
-                  ref: "Competition",
-                },
-                scores: [
-                  {
-                    challengeId: {
-                      type: mongoose.Schema.Types.ObjectId,
-                      ref: "Challenge",
-                    },
-                    score: {
-                      scores: [
-                        {
-                          type: mongoose.Schema.Types.ObjectId,
-                          ref: "Score",
-                        },
-                      ],
-                    },
-                  },
-                ],
-              },
-            ], */
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    { timestamps: true }
+    members: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        role: {
+          type: String,
+          enum: ["member", "leader"],
+          default: "member",
+        },
+      },
+    ],
+    totalScore: {
+      type: Number,
+      default: 0,
+    }
+    /*     competitions: [
+          {
+            competitionId: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Competition",
+            },
+            scores: [
+              {
+                challengeId: {
+                  type: mongoose.Schema.Types.ObjectId,
+                  ref: "Challenge",
+                },
+                score: {
+                  scores: [
+                    {
+                      type: mongoose.Schema.Types.ObjectId,
+                      ref: "Score",
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        ], */
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Team", teamSchema);

dc135d7a5b2d3d3acab177fe8f1c16b18234d469
Yassine Mili <ya.mili@icloud.com>
2025-04-04 18:14:12 +0100

    fixing the cors bug

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const app = express();
 const server = http.createServer(app);
 app.use(
   cors({
-    origin: "http://localhost:5173", // Set the frontend URL explicitly
+    origin: "http://localhost:5174", // Set the frontend URL explicitly
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
 );

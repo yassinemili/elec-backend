4523809be5c942c6e46647d783a6fffb636f6c18
Yassine Mili <ya.mili@icloud.com>
2025-04-24 00:27:31 +0100

    add the socket to origin

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://elec-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://elec-frontend.vercel.app", "https://socket-p0.vercel.app/"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']

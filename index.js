23be8bd614aefef20dbed4caf07ae058e5df0c27
Yassine Mili <ya.mili@icloud.com>
2025-04-24 03:02:22 +0100

    allow local origin

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = http.createServer(app);
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://elec-frontend.vercel.app", "https://socket-p0.onrender.com/"],
+    origin: ["http://localhost:5174", "https://elec-frontend.vercel.app", "https://socket-p0.onrender.com/"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']

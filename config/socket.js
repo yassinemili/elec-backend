f84da0ed59ae385fa3ca237cb2471640488adfcc
Yassine Mili <ya.mili@icloud.com>
2025-04-24 01:39:52 +0100

    socket origins

--- config/socket.js
+++ config/socket.js
@@ -6,10 +6,7 @@ module.exports = (server) => {
   io = new Server(server, {
     cors: {
       origin: [
-        "http://localhost:5173",  // Development frontend URL
         "https://elec-frontend.vercel.app",  // Production frontend URL
-        "https://socket-p0.vercel.app",  // Another frontend URL
-        "https://socket-p0.onrender.com"  // Socket server hosted on Render
       ],
       methods: ["GET", "POST"],
       credentials: true,
